576adc395f28c2d1e612f709b7ce5562ed313889
harkemoyo <markndeche91@gmail.com>
2023-06-20 07:54:23 +0300

    added change

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ const website = profile[2];
 console.log(firstName);
 console.log(lastName);
 console.log(website);
+
+// destructuring swap variable
+
+let firstitle = 'Oluwatobi';
+let com = 'CodeSweetly';
+
+[firstitle, com] = [com, firstitle];
+
+console.log(firstitle);
+console.log(com);
+
