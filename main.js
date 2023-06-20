573dd28931d20f0d4214aa0dd01afa7373b68000
harkemoyo <markndeche91@gmail.com>
2023-06-20 06:55:54 +0300

    added js

---
+++ main.js
@@ -0,0 +1,9 @@
+const profile = ['Oluwatobi', 'Sofela', 'codesweetly.com'];
+
+const firstName = profile[0];
+const lastName = profile[1];
+const website = profile[2];
+
+console.log(firstName);
+console.log(lastName);
+console.log(website);
